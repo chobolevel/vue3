f12f5943af1ce55c3058af278e8c899b6075d101
chobolevel <rodaka123@naver.com>
2024-01-03 11:52:21 +0900

    Vue 3강 : v-for 반복문

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="menu">
+    <a v-for="link in links" :key="link">{{ link }}</a>
+  </div>
   <div v-for="(product, index) in products" :key="index">
-    <p>{{ product.name }}</p>
+    <h4>{{ product.name }}</h4>
     <p>{{ product.price }} 만원</p>
   </div>
 </template>
@@ -11,6 +13,7 @@ export default {
   name: 'App',
   data() {
     return {
+      links: [ 'Home', 'Shop', 'About' ],
       products: [
         { name: '역삼동 원룸', price: 50 },
         { name: '천호동 원룸', price: 60 },
@@ -27,7 +30,16 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #bbc5cf;
-  margin-top: 60px;
+}
+
+.menu {
+  background-color: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>

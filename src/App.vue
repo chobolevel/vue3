5f88277bbadcc5e4f6954d63a17777b2ed04367a
chobolevel <rodaka123@naver.com>
2024-01-03 11:35:31 +0900

    Vue 2강 : 데이터바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-for="(product, index) in products" :key="index">
+    <p>{{ product.name }}</p>
+    <p>{{ product.price }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      products: [
+        { name: '역삼동 원룸', price: 50 },
+        { name: '천호동 원룸', price: 60 },
+        { name: '마로구 원룸', price: 70 },
+      ]
+    }
   }
 }
 </script>
@@ -20,7 +27,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #bbc5cf;
   margin-top: 60px;
 }
 </style>
